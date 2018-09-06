6fd9a525e2c170235d38b478186faa5548a05995
Dynamicwaker <438340913@qq.com>
2018-09-06 21:39:27 +0800

    修改包名和提交新的学习实践

---
+++ src/com/dynamic/algorithm/ch01/TestArray.java
@@ -0,0 +1,34 @@
+package com.dynamic.algorithm.ch01;
+
+/**
+ * @author <a herf="mailto:xinghj0308@gmail.cn">xinghuajian</a>
+ * @version Created on 2018/9/3.22:34
+ * @description
+ */
+
+public class TestArray {
+    public static void main(String[] args){
+        // 创建数组
+        int[] myArray = new int[30];
+
+        // 数组初始化
+        boolean[] booleanArray = new boolean[10];
+        byte[] byteArray = new byte[10];
+        char[] charArray = new char[10];
+        float[] floatArray = new float[10];
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+        short[] shortArray = new short[10];
+        long[] longArray = new long[10];
+
+        System.out.println(booleanArray[0]);
+        System.out.println(byteArray[0]);
+        System.out.println(charArray[0]);
+        System.out.println(floatArray[0]);
+        System.out.println(intArray[0]);
+        System.out.println(doubleArray[0]);
+        System.out.println(shortArray[0]);
+        System.out.println(longArray[0]);
+
+     }
+}
