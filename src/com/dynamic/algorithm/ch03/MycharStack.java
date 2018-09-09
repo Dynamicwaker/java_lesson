c64224255a4410cff714406bb76b2e942721590b
Dynamicwaker <438340913@qq.com>
2018-09-09 18:26:00 +0800

    1.栈和队列的java简单实现 2.修改md文件目录

---
+++ src/com/dynamic/algorithm/ch03/MycharStack.java
@@ -0,0 +1,48 @@
+package com.dynamic.algorithm.ch03;
+
+/**
+ * @author <a herf="mailto:xinghj0308@gmail.cn">xinghuajian</a>
+ * @version Created on 2018/9/8.16:37
+ * @description 字符栈
+ */
+
+public class MycharStack {
+
+    private int maxSize;
+
+    private char[] arr;
+    // 栈顶元素指针
+    private int top;
+
+    public MycharStack(int size) {
+        maxSize = size;
+        arr = new char[maxSize];
+        top = -1;
+    }
+
+    // 压入数据
+    public void push(char value) {
+        arr[++top] = value;
+    }
+
+    // 取数据
+    public char pop() {
+        return arr[top--];
+    }
+
+    // 访问栈顶元素
+    public long peek() {
+        return arr[top];
+    }
+
+    // 是否为空
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    // 是否已满
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+}
