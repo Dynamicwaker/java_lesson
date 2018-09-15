317ccc61f637e9be2a524b9f097b608f0214ddec
Dynamicwaker <438340913@qq.com>
2018-09-15 08:30:50 +0800

    1. 树的简单实现 2. 递归的使用 2. 修改md文件目录

--- src/com/dynamic/algorithm/ch05/Triangle.java
+++ src/com/dynamic/algorithm/ch05/Triangle.java
@@ -17,6 +17,12 @@ public class Triangle {
         System.out.println(triangleRecursion(2));
         System.out.println(triangleRecursion(3));
         System.out.println(triangleRecursion(4));
+        System.out.println("-----------------------------");
+        System.out.println(Fibonacci(1));
+        System.out.println(Fibonacci(2));
+        System.out.println(Fibonacci(3));
+        System.out.println(Fibonacci(4));
+        System.out.println(Fibonacci(5));
     }
 
     // 使用循环
@@ -35,6 +41,14 @@ public class Triangle {
             return 1;
         } else {
             return n + triangleRecursion(--n);
+        }
+    }
+
+    public static int Fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
     }
 }
