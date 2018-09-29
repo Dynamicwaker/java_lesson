3531cce0c338bb614430967e9ebdf4ef206d9d4b
Dynamicwaker <438340913@qq.com>
2018-09-29 23:25:58 +0800

    线程初探，wait()方法

--- src/com/dynamic/threadPic/ch01/Bank.java
+++ src/com/dynamic/threadPic/ch01/Bank.java
@@ -15,15 +15,35 @@ public class Bank {
         this.name = name;
     }
 
+    /*
+     * 如果一个进程获取锁之后运行bank实例中的desposit()方法，那么其他线程无法运行
+     * 这个实例中的desposit方法和withdraw方法，需要排队等待
+     * 线程的互斥机制称为监视，或许锁叫做拥有监视
+     * assert Thread.holdsLock(obj) 当前线程是否以获取某一对象的锁
+     * synchronized静态方法和synchronized代码块使用的锁是不一样的
+     *
+     * 要执行wait方法，线程必须持有锁，等待的时候释放其锁
+     */
     public synchronized void deposit(int m) {
+
         money += m;
     }
+    /*
+     * 等效于：
+     * public void desposit(int m){
+     *    synchronized(this){
+     *      money += m;
+     *    }
+     * }
+     */
 
     public synchronized boolean withdraw(int m) {
         if (money >= m) {
             money -= m;
+            // 取款成功
             return true;
         } else {
+            // 余额不足
             return false;
         }
 
