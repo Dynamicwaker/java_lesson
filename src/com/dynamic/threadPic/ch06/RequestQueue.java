6b9b6c1e6150f19bb5cf97ed5dc306b0054c09e0
Dynamicwaker <438340913@qq.com>
2018-10-14 19:35:29 +0800

    Java 多线程Guarded Suspension模式注释

--- src/com/dynamic/threadPic/ch06/RequestQueue.java
+++ src/com/dynamic/threadPic/ch06/RequestQueue.java
@@ -7,6 +7,7 @@ import java.util.Queue;
  * @author <a herf="mailto:xinghj0308@gmail.cn">xinghuajian</a>
  * @version Created on 2018/10/13.22:36
  * @description 请求队列，存放请求信息
+ * 特征：1.存在循环 2.存在条件检查 3.因为某种原因而“等待”
  */
 
 public class RequestQueue {
@@ -15,8 +16,10 @@ public class RequestQueue {
     // 会返回最先放入的请求，没有则等待，直到有线程putRequest
     public synchronized Request getRequest() {
         // 如果队列中有元素则返回头元素否则返回null
+        // 此处作为守护条件
         while (queue.peek() == null) {
             try {
+                // 等效于this.wait()
                 wait();
             } catch (InterruptedException e) {
 
@@ -27,6 +30,7 @@ public class RequestQueue {
     }
 
     public synchronized void putRequest(Request request) {
+        // 向队列的末尾添加一个请求
         queue.offer(request);
         notifyAll();
     }
