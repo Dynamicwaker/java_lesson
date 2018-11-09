23b0513e1c0dd738341a21c7e45b2bcc8a7a9577
Dynamicwaker <438340913@qq.com>
2018-11-09 23:21:19 +0800

    Java多线程死锁demo

---
+++ src/com/dynamic/threadPic/ch17/DeadLockDemo.java
@@ -0,0 +1,46 @@
+package com.dynamic.threadPic.ch17;
+
+/**
+ * @author <a herf="mailto:xinghj0308@gmail.cn">xinghuajian</a>
+ * @version Created on 2018/11/9.22:14
+ * @description 死锁演示demo
+ */
+
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
